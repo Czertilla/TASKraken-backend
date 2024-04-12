a5fe688f93eab526715772d67de533b5b57c8d46
Czertilla <blaztermapskorsak@gmail.com>
2024-04-12 19:02:12 +0300

    feature(units_of_work) add structure module add class StructureUOW

---
+++ src/units_of_work/structure.py
@@ -0,0 +1,9 @@
+from repositories.structures import StructureORM
+from units_of_work._unit_of_work import UnitOfWork
+
+class StructureUOW(UnitOfWork):
+    async def __aenter__(self):
+        rtrn = await super().__aenter__()
+        self.struct = StructureORM(self.session)
+        return rtrn
+
