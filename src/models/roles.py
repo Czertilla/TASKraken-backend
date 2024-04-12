ade5b3463c3c6706fb99fe7b4f1d2408440e1d51
Czertilla <blaztermapskorsak@gmail.com>
2024-04-12 15:47:30 +0300

    feature(models) add roles module add module roles:     add class RoleORM

---
+++ src/models/roles.py
@@ -0,0 +1,35 @@
+from typing import TYPE_CHECKING
+from uuid import UUID
+from database import Base
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey
+
+from models.report import ReportORM
+
+
+
+if TYPE_CHECKING:
+    from models.users import UserORM
+    from models.structures import StructureORM
+    from models.tasks import ProjectORM, TaskORM
+
+class RoleORM(Base):
+    __tablename__ = "roles"
+
+    user_id: Mapped[UUID|None] = mapped_column(ForeignKey("user.id", ondelete="SET NULL"))
+    name: Mapped[str]
+    structure_id: Mapped[UUID] = mapped_column(ForeignKey("structures.id", ondelete="CASCADE"))
+    chief_id: Mapped[UUID|None] = mapped_column(ForeignKey("roles.id"), nullable=True)
+
+    user: Mapped["UserORM"] = relationship(back_populates="roles")
+    chief: Mapped["RoleORM"] = relationship(back_populates="subordinates")
+    structure: Mapped["StructureORM"] = relationship(back_populates="staff")
+    
+    subordinates: Mapped[list["RoleORM"]] = relationship(back_populates="chief")
+    created_projects: Mapped[list["ProjectORM"]] = relationship(back_populates="creator")
+    created_tasks: Mapped[list["TaskORM"]] = relationship(back_populates="creator")
+    tasks: Mapped[list["TaskORM"]] = relationship(
+        back_populates="responsibles",
+        secondary="responsibilities"
+    )
+    reports: Mapped[list["ReportORM"]] = relationship(back_populates="responsible")
