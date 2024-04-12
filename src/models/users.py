ac47dc3bc69a8480ef7ac7463a3655de1b134571
Czertilla <blaztermapskorsak@gmail.com>
2024-04-12 15:50:11 +0300

    feature(models.users) update UserORM class

--- src/models/users.py
+++ src/models/users.py
@@ -1,6 +1,15 @@
+from typing import TYPE_CHECKING
+from uuid import UUID
+
+from sqlalchemy import ForeignKey
 from database import Base
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTableUUID
+
+from models.files import FolderORM
+
+if TYPE_CHECKING:
+    from models.roles import RoleORM
 
 
 
@@ -8,3 +17,6 @@ class UserORM(SQLAlchemyBaseUserTableUUID, Base):
     __tablename__ = "user"
 
     username: Mapped[str] = mapped_column(unique=True)
+    photo_folder_id: Mapped[UUID] = mapped_column(ForeignKey("folders.id"), default=FolderORM)
+
+    roles: Mapped[list["RoleORM"]] = relationship(back_populates="user")
