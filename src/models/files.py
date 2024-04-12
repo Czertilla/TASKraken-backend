5d10a74c47f14d3b59f0dba1e006bc49ab6e2363
Czertilla <blaztermapskorsak@gmail.com>
2024-04-12 15:45:23 +0300

    feature(models.files) update FileORM class

--- src/models/files.py
+++ src/models/files.py
@@ -1,11 +1,34 @@
+from typing import TYPE_CHECKING, Optional, Union
 from sqlalchemy import func
 from database import Base
-from sqlalchemy.orm import Mapped, mapped_column
 from datetime import datetime
+from uuid import UUID
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey, func
+
+if TYPE_CHECKING:
+    from models.tasks import TaskORM
+    from models.report import ReportORM
+
+class FolderORM(Base):
+    __tablename__ = "folders"
+    
+    head_folder_id: Mapped[UUID] = mapped_column(ForeignKey("folders.id", ondelete='CASCADE'), nullable=True)
+
+    head_folder: Mapped["FolderORM"] = relationship(back_populates="folders")
+
+    files: Mapped[list["FileORM"]] = relationship(back_populates="folder")
+    folders: Mapped[list["FolderORM"]] = relationship(back_populates=head_folder)
+    enclosure: Mapped[Union["TaskORM","ReportORM"]] = relationship(back_populates="folder")
+
 
 class FileORM(Base):
     __tablename__ = "files"
 
-    data: Mapped[bytes]
-    created: Mapped[datetime] = mapped_column(default=func.now())
-    changed: Mapped[datetime|None]
+    data: Mapped[bytes|None]
+    name: Mapped[str|None]   
+    folder_id: Mapped[UUID|None] = mapped_column(ForeignKey("folders.id", ondelete="CASCADE"))
+    created_at: Mapped[datetime] = mapped_column(default=func.now())
+    changed_at: Mapped[datetime|None] = mapped_column(onupdate=func.now())
+
+    folder: Mapped[Optional["FolderORM"]] = relationship(back_populates="files")
