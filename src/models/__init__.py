8b174e641186ea0daf518655ef4c231b77348be4
Czertilla <blaztermapskorsak@gmail.com>
2024-04-12 15:50:35 +0300

    chore(models) edit package imports

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,11 +1,10 @@
 from database import Base
 
-from .camerus import DependenciesOrm
-from .cameras import CameraOrm
-from .carowners import CarOwnerORM
-from .violations import ViolationORM
-from .files import FileORM
-
-from .cases import CaseORM
+from .checklists import ChecklistORM, CheckpointORM
+from .files import FileORM, FolderORM
+from .report import ReportORM
+from .responsibilities import ResponsibilityORM
+from .roles import RoleORM
+from .structures import StructureORM
+from .tasks import ProjectORM, TaskORM
 from .users import UserORM
-from .votes import VoteORM
