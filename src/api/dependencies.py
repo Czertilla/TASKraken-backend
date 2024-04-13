c0744c34af0915075cfa9cd41e88e0fec948cd52
Czertilla <blaztermapskorsak@gmail.com>
2024-04-13 21:55:33 +0300

    feature(api.dependencies) add StructUOWDep var

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -1,8 +1,9 @@
 from typing import Annotated
 
 from fastapi import Depends
+from units_of_work.structure import StructureUOW
 from units_of_work.user import UserUOW
 from utils.absract.unit_of_work import ABCUnitOfWork
 
-
+StructUOWDep = Annotated[ABCUnitOfWork, Depends(StructureUOW)]
 UsersUOWDep = Annotated[ABCUnitOfWork, Depends(UserUOW)]
