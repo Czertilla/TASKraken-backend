bf467597e0e924cea68ffeece4604daadc3686c9
Czertilla <blaztermapskorsak@gmail.com>
2024-04-16 14:59:12 +0300

    feature(units_of_work.structures) update uow

--- src/units_of_work/structure.py
+++ src/units_of_work/structure.py
@@ -1,9 +1,11 @@
-from repositories.structures import StructureORM
+from repositories.roles import RoleRepo
+from repositories.structures import StructureRepo
 from units_of_work._unit_of_work import UnitOfWork
 
 class StructureUOW(UnitOfWork):
     async def __aenter__(self):
         rtrn = await super().__aenter__()
-        self.structs = StructureORM(self.session)
+        self.structs = StructureRepo(self.session)
+        self.roles = RoleRepo(self.session)
         return rtrn
 
