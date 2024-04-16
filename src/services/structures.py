7c0f18ed365713e2b88f14d19ce330d87cff518d
Czertilla <blaztermapskorsak@gmail.com>
2024-04-16 15:33:15 +0300

    chore(services.structures) regist_organization

--- src/services/structures.py
+++ src/services/structures.py
@@ -1,5 +1,6 @@
 from logging import getLogger
 from uuid import UUID
+from models.roles import RoleORM
 from models.structures import StructureORM
 from models.users import UserORM
 from schemas.structures import SRegistOrganization
@@ -8,11 +9,19 @@ from utils.absract.service import BaseService
 logger = getLogger(__name__)
 
 class StructureService(BaseService):
-    async def regist_organization(self, founder: UserORM, org_data: SRegistOrganization):
-        (org_data := org_data.model_dump()).update({
-            "head_id": founder.id
-        })
+    async def regist_organization(self, user: UserORM, org_data: SRegistOrganization):
         async with self.uow:
-            await self.uow.structs.add_one(org_data)
-            await self.uow.commit()
+            org_data: dict = org_data.model_dump()
+            gen_dir_data: dict = {
+                "name": org_data.pop("gen_dir_name"),
+                "user_id": user.id
+            }
+            org: StructureORM = await self.uow.structs.get(
+                structure_id:= await self.uow.structs.add_one(org_data)
+            )
+            gen_dir_data.update({
+                "structure_id": structure_id
+            })
+            org.head_id = await self.uow.roles.add_one(gen_dir_data)
+            await self.uow.commit(True)
 
