6a8794198b7dc5bb1a63855b0bef3b0980193e72
Czertilla <blaztermapskorsak@gmail.com>
2024-04-18 01:01:58 +0300

    feature(repositories.roles) add get_for_info func update module src.repositories.roles:     add func get_for_info

--- src/repositories/roles.py
+++ src/repositories/roles.py
@@ -1,5 +1,6 @@
 from uuid import UUID
 from sqlalchemy import select
+from sqlalchemy.orm import selectinload, joinedload
 from database import BaseRepo
 from logging import getLogger
 
@@ -9,3 +10,11 @@ logger = getLogger(__name__)
 
 class RoleRepo(BaseRepo):
     model = RoleORM
+
+
+    async def get_for_info(self, id: UUID):
+        return await self.get_with_options(
+            id, 
+            (joinedload(self.model.structure),)
+        )
+    
