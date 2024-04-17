adf5fc03e8b75a1580a0f66d9eec8f1f9b5ef633
Czertilla <blaztermapskorsak@gmail.com>
2024-04-17 21:25:47 +0300

    feature(api.dependencies) add RoleUOWDep

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -1,9 +1,11 @@
 from typing import Annotated
 
 from fastapi import Depends
+from units_of_work.roles import RoleUOW
 from units_of_work.structure import StructureUOW
 from units_of_work.user import UserUOW
 from utils.absract.unit_of_work import ABCUnitOfWork
 
 StructUOWDep = Annotated[ABCUnitOfWork, Depends(StructureUOW)]
 UsersUOWDep = Annotated[ABCUnitOfWork, Depends(UserUOW)]
+RoleUOWDep = Annotated[ABCUnitOfWork, Depends(RoleUOW)]
