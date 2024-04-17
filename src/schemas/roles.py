1d826c70783085fb3b4ca625179fe558ce1b0546
Czertilla <blaztermapskorsak@gmail.com>
2024-04-18 00:58:38 +0300

    feature(schemas.roles) add many schemas update module src.schemas.roles:     add class SRoleCheckResponce     add class SGetRolePageRequest     add class SRoleInfo     add class SRolePage

--- src/schemas/roles.py
+++ src/schemas/roles.py
@@ -1,15 +1,51 @@
 from typing import Annotated
+from uuid import UUID
 from fastapi import Depends, Query
 from pydantic import BaseModel, Field
 
 from schemas.rights import SRoleRights
 from utils.enums import RejectRight, TaskSendVector
+from utils.enums.roles import CheckRoleStatus, ViewMode
 
 
 class SCreateVacancy(BaseModel):
-    name: Annotated[str, Query(max_length=25)]
+    name: Annotated[str, Query(max_length=64)]
     rights: Annotated[SRoleRights, Query(title='Vacancy rights')] = Depends()
     level: Annotated[int, Query(ge=0)]
 
     class Config:
         from_atributes = True
+
+
+class SRoleCheckResponce(BaseModel):
+    status = Annotated[CheckRoleStatus, Field()]
+
+    class Config:
+        from_atributes = True
+
+
+class SGetRolePageRequest(BaseModel):
+    role_id: UUID | None
+    target_id: UUID
+
+
+class SRoleInfo(BaseModel):
+    view_mode: Annotated[ViewMode, Field()]
+    
+    user_id: UUID | None
+    name: Annotated[str, Field(max_length=64)]
+    org_name: Annotated[str, Field(max_length=512)]
+    level: Annotated[int, Field(ge=0)]
+
+    class Config:
+        from_atributes = True
+
+
+class SRolePage(SRoleInfo):
+    struct_id: UUID
+    org_id: UUID
+    chief_id: UUID
+    subordinates_list: list[UUID]
+    rights: Annotated[SRoleRights, Field()]
+
+
