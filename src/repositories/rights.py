a2f517b5bc185aa0c6ab9ae1020862f7baf888f9
Czertilla <blaztermapskorsak@gmail.com>
2024-04-20 02:05:00 +0300

    feature(repositories.rights) add get_by_role_id f

--- src/repositories/rights.py
+++ src/repositories/rights.py
@@ -1,3 +1,6 @@
+from uuid import UUID
+
+from sqlalchemy import select
 from database import BaseRepo
 from logging import getLogger
 
@@ -7,3 +10,11 @@ logger = getLogger(__name__)
 
 class RightRepo(BaseRepo):
     model = RoleRightORM
+
+
+    async def get_by_role_id(self, role_id: UUID) -> model|None:
+        stmt = (
+            select(self.model)
+            .where(self.model.role_id==role_id)
+        )
+        return (await self.execute(stmt)).scalar_one_or_none()
