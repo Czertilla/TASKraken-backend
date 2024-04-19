897f6d30a30196217d79a3c4debaab3d90b6fa48
Czertilla <blaztermapskorsak@gmail.com>
2024-04-20 01:56:24 +0300

    feature(schemas.rights) add fields of rights

--- src/schemas/rights.py
+++ src/schemas/rights.py
@@ -2,16 +2,21 @@ from typing import Annotated
 from fastapi import Query
 from pydantic import BaseModel
 from utils.enums.abstract import AEnum
-from utils.enums.rights import RejectTaskRight, RightsTemplateName, SendTaskVector
+from utils.enums.rights import CreateStructRight, CreateVacancyRigth, EditOtherRight, RejectTaskRight, RightsTemplateName, SendPetitionVector, SendTaskVector
 
 
 class SRoleRights(BaseModel):
     template: Annotated[RightsTemplateName, Query(default=RightsTemplateName.__default__)]
-    can_create_substructures: Annotated[bool, Query(default=False)]
-    can_create_subordinates: Annotated[bool, Query(default=False)]
+    can_create_substructures: Annotated[CreateStructRight, Query(default=CreateStructRight.__default__)]
+    can_create_subordinates: Annotated[CreateVacancyRigth, Query(default=CreateVacancyRigth.__default__)]
     can_send_task: Annotated[SendTaskVector, Query(default=SendTaskVector.__default__)]
     can_send_report: Annotated[bool, Query(default=True)]
+    can_send_petition: Annotated[SendPetitionVector, Query(default=SendPetitionVector.__default__)]
     can_reject_task: Annotated[RejectTaskRight, Query(default=RejectTaskRight.__default__)]
+    can_create_project: Annotated[bool, Query(default=False)]
+    can_edit_other_rights: Annotated[EditOtherRight, Query(default=EditOtherRight.__default__)]
+    can_edit_oneself_rights: Annotated[bool, Query(default=False)]
+
 
     class Config:
         from_attributes = True
@@ -19,16 +24,19 @@ class SRoleRights(BaseModel):
 
 class SGenDirRights(SRoleRights):
     template: Annotated[RightsTemplateName, Query(default=RightsTemplateName.gendir)]
-    can_create_substructures: Annotated[bool, Query(default=True)]
-    can_create_subordinates: Annotated[bool, Query(default=True)]
-    can_send_task: Annotated[SendTaskVector, Query(default=SendTaskVector.organization)]
+    can_create_substructures: Annotated[CreateStructRight, Query(default=CreateStructRight.in_organization)]
+    can_create_subordinates: Annotated[CreateVacancyRigth, Query(default=CreateVacancyRigth.organization)]
+    can_send_task: Annotated[SendTaskVector, Query(default=SendTaskVector.everyone)]
+    can_send_report: Annotated[bool, Query(default=True)]
+    can_send_petition: Annotated[SendPetitionVector, Query(default=SendPetitionVector.everyone)]
     can_reject_task: Annotated[RejectTaskRight, Query(default=RejectTaskRight.everyone)]
+    can_create_project: Annotated[bool, Query(default=True)]
+    can_edit_other_rights: Annotated[EditOtherRight, Query(default=EditOtherRight.organization)]
+    can_edit_oneself_rights: Annotated[bool, Query(default=True)]
 
 
 class SHeadRights(SRoleRights):
     template: Annotated[RightsTemplateName, Query(default=RightsTemplateName.head)]
-    can_create_substructures: Annotated[bool, Query(default=True)]
-    can_create_subordinates: Annotated[bool, Query(default=True)]
-    can_send_task: Annotated[SendTaskVector, Query(default=SendTaskVector.structure)]
-    can_reject_task: Annotated[RejectTaskRight, Query(default=RejectTaskRight.same_level)]
-
+    can_create_subordinates: Annotated[CreateVacancyRigth, Query(default=CreateVacancyRigth.organization)]
+    can_edit_other_rights: Annotated[EditOtherRight, Query(default=EditOtherRight.organization)]
+    can_edit_oneself_rights: Annotated[bool, Query(default=True)]
