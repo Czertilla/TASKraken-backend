2b3fe193879cb41e99d659cb2af7a0fd60981d65
Czertilla <blaztermapskorsak@gmail.com>
2024-04-20 01:55:34 +0300

    fix(units_of_work.roles) add more repositories

--- src/units_of_work/roles.py
+++ src/units_of_work/roles.py
@@ -1,4 +1,6 @@
 from repositories.files import FileRepo
+from repositories.rights import RightRepo
+from repositories.roles import RoleRepo
 from repositories.users import UserRepo
 from units_of_work._unit_of_work import UnitOfWork
 
@@ -6,5 +8,7 @@ class RoleUOW(UnitOfWork):
     async def __aenter__(self):
         rtrn = await super().__aenter__()
         self.files = FileRepo(self.session)
-        self.roles = UserRepo(self.session)
+        self.roles = RoleRepo(self.session)
+        self.users = UserRepo(self.session)
+        self.rights = RightRepo(self.session)
         return rtrn
