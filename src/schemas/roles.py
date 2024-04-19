86ac1f28595cec6d2ea011c54b247cd192846bde
Czertilla <blaztermapskorsak@gmail.com>
2024-04-20 02:02:35 +0300

    fix(schemas.roles) add None optionals

--- src/schemas/roles.py
+++ src/schemas/roles.py
@@ -1,4 +1,4 @@
-from typing import Annotated
+from typing import Annotated, Optional, Union
 from uuid import UUID
 from fastapi import Depends, Query
 from pydantic import BaseModel, Field
@@ -10,6 +10,7 @@ from utils.enums.roles import CheckRoleStatus, ViewMode
 class SCreateVacancy(BaseModel):
     name: Annotated[str, Query(max_length=64)]
     rights: Annotated[SRoleRights, Query(title='Vacancy rights')] = Depends()
+    chief_id: UUID
     level: Annotated[int, Query(ge=0)]
 
     class Config:
@@ -24,7 +25,7 @@ class SRoleCheckResponce(BaseModel):
 
 
 class SGetRolePageRequest(BaseModel):
-    role_id: UUID | None
+    role_id: UUID | None = None
     target_id: UUID
 
 
@@ -43,7 +44,7 @@ class SRoleInfo(BaseModel):
 class SRolePage(SRoleInfo):
     struct_id: UUID
     org_id: UUID
-    chief_id: UUID
+    chief_id: UUID | None
     subordinates_list: list[UUID]
     rights: Annotated[SRoleRights, Field()]
 
