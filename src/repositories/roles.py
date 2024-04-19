91f3bfcf56c225b6cb6c318e4cfb09ed1bc7f3fc
Czertilla <blaztermapskorsak@gmail.com>
2024-04-20 02:06:10 +0300

    feature(repositories.roles) add get_with_rights f

--- src/repositories/roles.py
+++ src/repositories/roles.py
@@ -1,3 +1,4 @@
+from typing import Type
 from uuid import UUID
 from sqlalchemy import select
 from sqlalchemy.orm import selectinload, joinedload
@@ -5,6 +6,7 @@ from database import BaseRepo
 from logging import getLogger
 
 from models import RoleORM
+from models.structures import StructureORM
 
 logger = getLogger(__name__)
 
@@ -15,7 +17,10 @@ class RoleRepo(BaseRepo):
     async def get_for_info(self, id: UUID):
         return await self.get_with_options(
             id, 
-            (joinedload(self.model.structure),)
+            (
+                joinedload(self.model.structure)
+                .joinedload(StructureORM.org),
+            )
         )
     
 
@@ -23,8 +28,18 @@ class RoleRepo(BaseRepo):
         return await self.get_with_options(
             id,
             (
-                joinedload(self.model.structure),
+                joinedload(self.model.structure)
+                .joinedload(StructureORM.org),
                 selectinload(self.model.subordinates),
                 joinedload(self.model.rights)
              )
+        )
+
+
+    async def get_with_rights(self, id: UUID):
+        return await self.get_with_options(
+            id,
+            (
+                joinedload(self.model.rights),
+            )
         )
