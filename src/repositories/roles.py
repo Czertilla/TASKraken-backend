d8b6a72fc633a553ec3583b9642a9f167fcbe7be
Czertilla <blaztermapskorsak@gmail.com>
2024-04-21 21:58:15 +0300

    feature(repositories.roles) add is_on_downstream f update module src.repositories.roles:     update class RoleRepo:         add method is_on_downstream

--- src/repositories/roles.py
+++ src/repositories/roles.py
@@ -7,6 +7,7 @@ from logging import getLogger
 
 from models import RoleORM
 from models.structures import StructureORM
+from utils.enums.roles import DownstreamStatus
 
 logger = getLogger(__name__)
 
@@ -43,3 +44,47 @@ class RoleRepo(BaseRepo):
                 joinedload(self.model.rights),
             )
         )
+    
+
+    async def is_on_downstream(
+        self, 
+        first_id: UUID, 
+        second_id: UUID,
+        reverse_possible: bool = False,
+        /,
+        first_call: bool = True,
+        first_level: int = None,
+        is_reversed: bool = False
+    ) -> DownstreamStatus:
+        second_role: RoleORM = (await self.get_with_options(
+            second_id, 
+            (joinedload(self.model.chief))
+        ))
+        if first_call:
+            first_role: RoleORM = self.get(first_id)
+            first_level = first_role.level
+            if first_id is None or second_id is None:
+                if first_id is not None:
+                    return DownstreamStatus.seconds_invalid
+                elif second_id is not None:
+                    return DownstreamStatus.first_invalid
+                else:
+                    return DownstreamStatus.both_invalid
+        if first_level == second_role.level:
+            return DownstreamStatus.false
+        elif first_level < second_role.level:
+            if reverse_possible:
+                return await self.is_on_downstream(second_id, first_id, is_reversed=True)
+            else:
+                return DownstreamStatus.false
+        if second_role.chief_id == first_id:
+            return DownstreamStatus.upstream if is_reversed else DownstreamStatus.true
+        return await self.is_on_downstream(
+            first_id, 
+            second_role.chief_id, 
+            reverse_possible,
+            first_call=False,
+            first_level=first_level,
+            is_reversed=is_reversed
+        )
+        
