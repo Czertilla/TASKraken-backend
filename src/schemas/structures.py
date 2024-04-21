ec450ff72a5889bf38eb95af49788f6639915bfb
Czertilla <blaztermapskorsak@gmail.com>
2024-04-21 23:21:26 +0300

    feature(schemas.structures) add SCreateStruct cls update module src.schemas:     add class SCreateStruct     update class SRegistOrganization:         set extend SCreateStruct

--- src/schemas/structures.py
+++ src/schemas/structures.py
@@ -1,6 +1,6 @@
 
 from datetime import datetime
-from typing import Annotated
+from typing import Annotated, ClassVar
 from uuid import UUID
 from fastapi import Depends, Query
 from pydantic import BaseModel, Field
@@ -8,14 +8,19 @@ from pydantic import BaseModel, Field
 from schemas.rights import SGenDirRights
 
 
-class SRegistOrganization(BaseModel):
+class SCreateStruct(BaseModel):
     name: Annotated[str, Query(max_length=64)]
     desctription: Annotated[str, Query(max_length=512)]
-    gen_dir_name: Annotated[str, Query(max_length=64)]
-    rights: Annotated[SGenDirRights, Query(title="Gen-Dir Rights")] = Depends()
+    enclosure_id: Annotated[UUID, Query(title="id of overstruct")]
 
     class Config:
         from_atributes = True
+
+
+class SRegistOrganization(SCreateStruct):
+    gen_dir_name: Annotated[str, Query(max_length=64)]
+    rights: Annotated[SGenDirRights, Query(title="Gen-Dir Rights")] = Depends()
+    enclosure_id: ClassVar[None]
 
 
 class SRegistOrgResponse(BaseModel):
