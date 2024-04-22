95461a48a65fbbce6edb397c49b41b9f8e2d86b8
Czertilla <blaztermapskorsak@gmail.com>
2024-04-23 01:42:19 +0300

    feature(routers.roles) add search/my_roles funcs

--- src/api/routers/roles.py
+++ src/api/routers/roles.py
@@ -1,18 +1,41 @@
 from typing import Annotated
 from uuid import UUID
 from fastapi import APIRouter, Depends
+from fastapi_filter import FilterDepends
 
 from api.auth.auth import fastapi_users
 from api.dependencies import RoleUOWDep, StructUOWDep
+from filters.roles import RoleFilter
 from models.users import UserORM
+from schemas.pagination import SPaginationRequest
 from schemas.rights import SRoleRights
-from schemas.roles import SCreateRoleResponse, SCreateStructHead, SCreateSubordinate, SGetRolePageRequest, SRoleCheckResponce, SRoleInfo, SRolePage
+from schemas.roles import SCreateRoleResponse, SCreateStructHead, SCreateSubordinate, SGetRolePageRequest, SRoleCheckResponce, SRoleInfo, SRolePage, SRoleSearchResponse
 from services.roles import RoleService
 
 get_verified = fastapi_users.current_user(verified=True, active=True)
 get_optional = fastapi_users.current_user(optional=True)
 
 roles = APIRouter(prefix="/role", tags=["roles"])
+
+
+@roles.get("/search")
+async def search_roles(
+    uow: RoleUOWDep,
+    user: UserORM = Depends(get_optional),
+    filters: RoleFilter = FilterDepends(RoleFilter),
+    pagination: SPaginationRequest = Depends()
+) -> SRoleSearchResponse:
+    return await RoleService(uow).search(filters, pagination)
+
+
+@roles.get("/home")
+async def my_roles(
+    uow: RoleUOWDep,
+    user: UserORM = Depends(get_verified),
+    pagination: SPaginationRequest = Depends()
+) -> SRoleSearchResponse:
+    return await RoleService(uow).search(RoleFilter(user_id=user.id), pagination)
+
 
 @roles.get("/{target_id}")
 async def get_role_page(
