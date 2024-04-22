dbadbc98c56e1aa0bf1dd197abe25e975d0aae86
Czertilla <blaztermapskorsak@gmail.com>
2024-04-23 01:32:57 +0300

    feature(abstract.service) add get_offset method

--- src/utils/absract/service.py
+++ src/utils/absract/service.py
@@ -1,6 +1,13 @@
+from schemas.pagination import SPaginationRequest
 from utils.absract.unit_of_work import ABCUnitOfWork
 
 
 class BaseService:
     def __init__(self, uow: ABCUnitOfWork) -> None:
         self.uow = uow
+
+    def get_offset(self, pagination: SPaginationRequest)->tuple[int]:
+        return (
+            pagination.page * pagination.size,
+            (pagination.page + 1) * pagination.size
+        )
