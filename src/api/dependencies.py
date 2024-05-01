25e13270882e0a4e116326cc2beab43e58026267
Czertilla <blaztermapskorsak@gmail.com>
2024-05-01 23:26:52 +0300

    chore(api.dependencies) add ProjectUOW, TaskUOW

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -1,9 +1,11 @@
 from typing import Annotated
+from uuid import UUID
 
 from fastapi import Cookie, Depends
 from units_of_work.projects import ProjectUOW
 from units_of_work.roles import RoleUOW
 from units_of_work.structure import StructureUOW
+from units_of_work.tasks import TaskUOW
 from units_of_work.user import UserUOW
 from utils.absract.unit_of_work import ABCUnitOfWork
 
@@ -14,3 +16,5 @@ ProjectUUID = Annotated[UUID, Cookie()]
 StructUOWDep = Annotated[ABCUnitOfWork, Depends(StructureUOW)]
 UsersUOWDep = Annotated[ABCUnitOfWork, Depends(UserUOW)]
 RoleUOWDep = Annotated[ABCUnitOfWork, Depends(RoleUOW)]
+ProjectUOWDep = Annotated[ABCUnitOfWork, Depends(ProjectUOW)]
+TaskUOWDep = Annotated[ABCUnitOfWork, Depends(TaskUOW)]
