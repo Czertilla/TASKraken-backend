f7307659b444b7203873e2b2a98ac668529f6b54
Czertilla <blaztermapskorsak@gmail.com>
2024-05-01 23:21:24 +0300

    feature(units_of_work) add units of work update module src.utils.absract.unit_of_work:     add imports     update class ABCUnitOfWork:         add field folders         add field projects         add field tasks update module src.unit_of_work.all:     add imports     update class AllUOW:         add field roles         add field right         add field structs         add field projects         add field tasks add module src.unit_of_work.tasks:     add class TaskUOW

--- src/units_of_work/tasks.py
+++ src/units_of_work/tasks.py
@@ -1,23 +1,15 @@
 from repositories.files import FileRepo, FolderRepo
-from repositories.projects import ProjectRepo
 from repositories.rights import RightRepo
 from repositories.roles import RoleRepo
-from repositories.structures import StructureRepo
 from repositories.tasks import TaskRepo
 from repositories.users import UserRepo
 from units_of_work._unit_of_work import UnitOfWork
 
-class AllUOW(UnitOfWork):
+class TaskUOW(UnitOfWork):
     async def __aenter__(self):
         rtrn = await super().__aenter__()
-        
         self.files = FileRepo(self.session)
         self.folders = FolderRepo(self.session)
-        self.users = UserRepo(self.session)
         self.roles = RoleRepo(self.session)
-        self.rights = RightRepo(self.session)
-        self.structs = StructureRepo(self.session)
-        self.projects = ProjectRepo(self.session)
         self.tasks = TaskRepo(self.session)
-        
         return rtrn
--- src/utils/absract/unit_of_work.py
+++ src/utils/absract/unit_of_work.py
@@ -1,19 +1,24 @@
 from abc import ABC, abstractmethod
 from typing import Type
 
-from repositories.files import FileRepo
+from repositories.files import FileRepo, FolderRepo
+from repositories.projects import ProjectRepo
 from repositories.rights import RightRepo
 from repositories.roles import RoleRepo
 from repositories.structures import StructureRepo
+from repositories.tasks import TaskRepo
 from repositories.users import UserRepo
 
 
 class ABCUnitOfWork(ABC):
     files: Type[FileRepo]
+    folders: Type[FolderRepo]
     users: Type[UserRepo]
     roles: Type[RoleRepo]
     rights: Type[RightRepo]
     structs: Type[StructureRepo]
+    projects: Type[ProjectRepo]
+    tasks: Type[TaskRepo]
 
     @abstractmethod
     def __init__(self):
